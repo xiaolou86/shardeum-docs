328911669bbd5b206ffe9f73107c06ce57c80e14
anvaynk <anvaynk@gmail.com>
2022-04-26 00:00:14 +0530

    dev: Fixed the github editPath config.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/shardeum/shardeum-docs',
+          editUrl: 'https://github.com/Shardeum/shardeum-docs/tree/master/',
         },
         blog:false,
         theme: {

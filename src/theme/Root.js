8f7099ba15a1c4c07c26ad699a013993e2330c76
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2022-05-03 16:01:34 -0500

    add network reset notification

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -4,19 +4,24 @@ import Polipop from 'polipop';
 // Default implementation, that you can customize
 export default function Root({children}) {
 
-//   var polipop = new Polipop('mypolipop', {
-//       layout: 'popups',
-//       position: 'bottom-right',
-//       progressbar: true,
-//       sticky: true,
-//
-//   });
-//
-//   polipop.add({
-//     content: 'Please be aware that Shardeum Liberty may be reset within the next 24 hours for performance updates. If there is some data that you want to pull from your DApps, we suggest doing it now to make sure your data is not lost in the event of a full network re-launch.',
-//     title: 'Upcoming Reset',
-//     type: 'default',
-// });
+  var polipop = new Polipop('mypolipop', {
+      layout: 'popups',
+      position: 'bottom-right',
+      progressbar: true,
+      sticky: true,
+
+  });
+
+
+
+  polipop.add({
+    content: `<b>Reminder:</b> The Liberty 1.0 network will be reset on May 4th, 2022 at 5:00 PM UTC.
+    <br>
+          <br>
+    Accounts that received SHM from the Faucet through a tweet will be automatically given funds after the reset.`,
+    title: 'Upcoming Reset',
+    type: 'default',
+});
 
   return <>{children}</>;
 

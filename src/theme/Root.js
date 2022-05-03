f725618489e9c65f530cf446e88a52130f4e0279
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2022-05-03 16:50:31 -0500

    add network reset notification

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -5,26 +5,22 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 // Default implementation, that you can customize
 export default function Root({children}) {
 
-
   if (ExecutionEnvironment.canUseDOM) {
-  var polipop = new Polipop('mypolipop', {
+    var polipop = new Polipop('mypolipop', {
       layout: 'popups',
       position: 'bottom-right',
       progressbar: true,
-      sticky: true,
+      sticky: true
+    });
 
-  });
-
-  polipop.add({
-    content: `<b>Reminder:</b> The Liberty 1.0 network will be reset on May 4th, 2022 at 5:00 PM UTC.
+    polipop.add({content: `<b>Reminder:</b> The Liberty 1.0 network will be reset on May 4th, 2022 at 5:00 PM UTC.
     <br>
-          <br>
-    Accounts that received SHM from the Faucet through a tweet will be automatically given funds after the reset.`,
-    title: 'Upcoming Reset',
-    type: 'default',
-});
-}
+    <br>
+    Accounts that received SHM from the Faucet through a tweet will be automatically given funds after the reset.`, title: 'Upcoming Reset', type: 'default'});
+  }
 
-  return <>{children}</>;
+  return <> {
+    children
+  }</>;
 
 }

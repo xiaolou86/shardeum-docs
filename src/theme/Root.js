7f66c1454b183254d53abeaa317e98660fbcbb87
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2022-05-05 13:15:03 -0500

    remove reset banner

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -5,19 +5,19 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 // Default implementation, that you can customize
 export default function Root({children}) {
 
-  if (ExecutionEnvironment.canUseDOM) {
-    var polipop = new Polipop('mypolipop', {
-      layout: 'popups',
-      position: 'bottom-right',
-      progressbar: true,
-      sticky: true
-    });
-
-    polipop.add({content: `<b>Reminder:</b> The Liberty 1.0 network will be reset on May 5th, 2022 at 3:00 PM UTC.
-    <br>
-    <br>
-    Accounts that received SHM from the Faucet through a tweet will be automatically given funds after the reset.`, title: 'Upcoming Reset', type: 'default'});
-  }
+  // if (ExecutionEnvironment.canUseDOM) {
+  //   var polipop = new Polipop('mypolipop', {
+  //     layout: 'popups',
+  //     position: 'bottom-right',
+  //     progressbar: true,
+  //     sticky: true
+  //   });
+  //
+  //   polipop.add({content: `<b>Reminder:</b> The Liberty 1.0 network will be reset on May 5th, 2022 at 3:00 PM UTC.
+  //   <br>
+  //   <br>
+  //   Accounts that received SHM from the Faucet through a tweet will be automatically given funds after the reset.`, title: 'Upcoming Reset', type: 'default'});
+  // }
 
   return <> {
     children

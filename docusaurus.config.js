c6c71e6a86a92ec1c9a874b2aded619e6379de22
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2022-05-12 18:28:28 -0500

    added image zoom

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
      // Optional medium-zoom options
      // see: https://www.npmjs.com/package/medium-zoom#options
      options: {
-       margin: 20,
+       margin: 40,
        background: '#000',
        scrollOffset: 60,
        // container: 'main',

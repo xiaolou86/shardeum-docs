7576617980d9378307e63188a88ac3247dbe9c39
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2022-05-21 16:55:10 +0100

    added faucet explorer

---
+++ src/components/FaucetDataDay.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class transactionList extends React.Component {
+  state = {
+    transactions: [],
+    cycle: [],
+    cycleFrom: [],
+    cycleTo: [],
+  }
+
+  componentDidMount() {
+    axios.get(`https://explorer.liberty10.shardeum.org/api/cycleinfo?count=1`).then(res => {
+      const cycle = res.data;
+      this.setState({cycle});
+      const currentCycle = this.state.cycle.cycles[0].counter;
+      const cycleFrom = Math.max(0,currentCycle - this.props.timestampFrom);
+      const cycleTo = currentCycle - this.props.timestampTo;
+      this.setState({cycleFrom});
+      this.setState({cycleTo});
+      axios.get(`https://explorer.liberty10.shardeum.org/api/transaction?startCycle=${cycleFrom}&endCycle=${cycleTo}&address=0x1f1545Eb7EE5C3C1c4784ee9ddE5D26A9f76F77C`).then(res => {
+        const transactions = res.data.transactions;
+        this.setState({transactions});
+      })
+    })
+  }
+
+  render() {
+    if(this.state.transactions === undefined){
+    return (<div>Faucet Claims: 0 </div>)
+  }
+  else {
+    return (<div>
+      <p>Faucet Claims: {this.state.transactions.length}</p>
+      <p>SHM Issued: {this.state.transactions.length *100} </p>
+      <p>Cycle Range: {this.state.cycleFrom} - {this.state.cycleTo}  </p>
+    </div>)
+  }
+}
+}
---
+++ src/components/FaucetExplorer.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+import FaucetDataCustom from '@site/src/components/FaucetDataCustom.js';
+import FaucetDataDay from '@site/src/components/FaucetDataDay.js';
+import FaucetDataTotal from '@site/src/components/FaucetDataTotal.js';
+import timestamp from 'unix-timestamp';
+import dayjs from 'dayjs';
+
+
+
+export default function datePicker() {
+  timestamp.round = true
+
+  const [value, onChange] = useState([dayjs().startOf('day').toString(), new Date()]);
+  const timeNow = Math.round(timestamp.now() / 60);
+  const timestampFrom = Math.round(timeNow - timestamp.fromDate(value[0]) / 60);
+  const timestampTo= Math.max(0, Math.round(timeNow - timestamp.fromDate(value[1]) / 60));
+  const startOfDay = dayjs().startOf('day').toString();
+
+
+  return (
+
+    <div>
+      <div class="faucetContainer">
+        <div class="faucetItem">
+          <h3>Day</h3>
+          <FaucetDataDay timestampFrom={timestampFrom} timestampTo ={timestampTo} timeNow={timeNow} onChange={onChange}/>
+
+        </div>
+        <div class="faucetItem">
+          <h3>Total</h3>
+          <FaucetDataTotal timestampFrom={timestampFrom} timestampTo ={timestampTo} timeNow={timeNow} onChange={onChange}/>
+        </div>
+        <div class="faucetItem">
+          <h3>Custom Range</h3>
+        <FaucetDataCustom timestampFrom={timestampFrom} timestampTo ={timestampTo} timeNow={timeNow} onChange={onChange}/>
+
+            <DateRangePicker onChange={onChange} value={value} minDate={new Date('2022-05-05')} maxDate={new Date()}/>
+        </div>
+    </div>
+
+    </div>
+
+);
+
+}

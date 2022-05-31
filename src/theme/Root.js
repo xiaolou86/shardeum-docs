f687c5a9f5492f231ac478ed628d6ee4a1b9ebec
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2022-05-31 01:31:18 +0100

    1.1 restart

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -5,19 +5,19 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 // Default implementation, that you can customize
 export default function Root({children}) {
 
-  // if (ExecutionEnvironment.canUseDOM) {
-  //   var polipop = new Polipop('mypolipop', {
-  //     layout: 'popups',
-  //     position: 'bottom-right',
-  //     progressbar: true,
-  //     sticky: true
-  //   });
-  //
-  //   polipop.add({content: `<b>Reminder:</b> Shardeum Liberty 1.1 will launch on May 25th, 2022 at 2:00 PM UTC.
-  //   <br>
-  // <br>
-  //   *Liberty 1.0 will be retired, and all account data may be lost. Accounts that received SHM from the Faucet through a tweet will automatically be given funds on Liberty 1.1.`, title: 'Liberty 1.1 Launch', type: 'default'});
-  // }
+  if (ExecutionEnvironment.canUseDOM) {
+    var polipop = new Polipop('mypolipop', {
+      layout: 'popups',
+      position: 'bottom-right',
+      progressbar: true,
+      sticky: true
+    });
+
+    polipop.add({content: `<b>Reminder:</b> The Shardeum network was restarted on May 30th, 2022 at 10:00 AM UTC due to malicious activity on the network.
+    <br>
+  <br>
+    *Network data was not preserved, and contracts will need to be redeployed; accounts that tweeted to receive SHM should retain the same balance`, title: 'Liberty 1.1 Restart', type: 'default'});
+  }
 
   return <> {
     children

628bfc4cfe0e15c723ee9c99eb1728dae03895f2
Daniel Ingamells <dan_ingamells@hotmail.co.uk>
2022-06-09 11:13:04 -0500

    network reboot

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -5,19 +5,16 @@ import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 // Default implementation, that you can customize
 export default function Root({children}) {
 
-  // if (ExecutionEnvironment.canUseDOM) {
-  //   var polipop = new Polipop('mypolipop', {
-  //     layout: 'popups',
-  //     position: 'bottom-right',
-  //     progressbar: true,
-  //     sticky: true
-  //   });
-  //
-  //   polipop.add({content: `<b>Reminder:</b> The Shardeum network was restarted on May 30th, 2022 at 10:00 AM UTC due to malicious activity on the network.
-  //   <br>
-  // <br>
-  //   *Network data was not preserved, and contracts will need to be redeployed; accounts that tweeted to receive SHM should retain the same balance`, title: 'Liberty 1.1 Restart', type: 'default'});
-  // }
+  if (ExecutionEnvironment.canUseDOM) {
+    var polipop = new Polipop('mypolipop', {
+      layout: 'popups',
+      position: 'bottom-right',
+      progressbar: true,
+      sticky: true
+    });
+
+    polipop.add({content: `<b>Reminder:</b> The Liberty Testnet will be rebooted at 11:00 AM Central Time. The reboot will take about 4 hours. All data will be preserved`, title: 'Liberty 1.1 Reboot', type: 'default'});
+  }
 
   return <> {
     children
